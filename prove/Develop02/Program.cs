695df987c2346625eccbe1e1572f99fb857e9467
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-05-11 14:56:49 -0600

    Final additions to Journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,6 +2,19 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
+/*
+    Additions:
+    1 - I added a word count and character count for each entry.
+    2 - I added the ability for the program to understand that a 
+        trailing space does not count as an extra word. 
+    3 - I also added logic to ensure that if someone added a space 
+        before a !, ?, or period, that it wouldn't count that as
+        another word.
+
+    Example: "Entry: Amazing Work !", will count as 2 words, 
+            not 3 because of the space before the exclamation mark.
+
+*/
 class Program
 {
     static void Main(string[] args)
@@ -10,6 +23,7 @@ class Program
         List<string> to_be_saved = new List<string>();
         while(keep_going)
         {
+            //tell the user their options
             Console.WriteLine("Please select one of the folowing options:");
             Console.WriteLine("1. Write");
             Console.WriteLine("2. Display");
@@ -17,21 +31,30 @@ class Program
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
             Console.Write("> ");
+            //turn their input into an integer
             int input = Convert.ToInt32(Console.ReadLine());
 
+            //create an instance of the Write Class
             Write write = new Write();
             
-
+            //determine which input was selected
             switch(input)
             {
+                //Write
                 case 1:
-                    Console.WriteLine("Starting write");
-
+                    //get a random prompt from list
                     string prompt = write.rand_prompt();
+                    //take in the entry
                     string entry = write.take_entry();
+                    //count the words and characters
+                    int w_count = write.word_count();
+                    int c_count = write.char_count(); 
+                    //instatiate the day and time
                     DateTime currentDateTime = DateTime.Now;
-                    to_be_saved.Add("Date: " + currentDateTime + " - Prompt: " + prompt + "\r\nEntry: " + entry + "\r\n");
+                    //create string that will be saved in file
+                    to_be_saved.Add("Date: " + currentDateTime + " - Prompt: " + prompt + "\r\nWord Count: " + w_count + " Character Count: " + c_count + "\r\nEntry: " + entry + "\r\n");
                     break;
+                //Display
                 case 2:
                     for(int i = 0; i < to_be_saved.Count; i++)
                     {
@@ -40,20 +63,24 @@ class Program
                         Console.WriteLine();
                     }
                     break;
+                //Load
                 case 3:
                     Load load = new Load();
                     string load_file = load.loading();
                     to_be_saved.Add(File.ReadAllText(load_file));
                     break;
+                //Save
                 case 4:
                     Save save = new Save();
                     string file = save.Saving();
                     System.IO.File.WriteAllLines(file, to_be_saved);
                     break;
+                //Quit
                 case 5:
                     Console.WriteLine("Quitting...");
                     keep_going = false;
                     break;
+                //Just in case someone divides by zero...
                 default:
                     Console.WriteLine("this is default");
                     break;
--- prove/Develop02/Write.cs
+++ prove/Develop02/Write.cs
@@ -4,12 +4,16 @@ public class Write
     public string[] prompts = {"If I could do one thing different today, what would it be?", "What was the best part of my day?", 
                             "Who was the most interesting person I interacted with today?","What was the best part of my day?", 
                             "How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?",
-                            "If I had one thing I could do over today, what would it be?"};    
+                            "If I had one thing I could do over today, what would it be?"};
+    
+    //declairing entry global variable 
+    //so I can use it in two places.
+    string entry;
     
     public string take_entry()
     {
         Console.Write("> ");
-        string entry = Console.ReadLine();
+        entry = Console.ReadLine();
         return entry;
     }
     public string rand_prompt()
@@ -20,5 +24,45 @@ public class Write
         Console.WriteLine(prompts[rand_num]);
 
         return prompts[rand_num];
+    }
+    public int word_count()
+    {
+        int count = 1;
+        for(int i = 0; i < entry.Length; i++)
+        {
+            //check for spaces or new lines
+            if(entry[i] == ' ' || entry[i] == '\n' || entry[i] == '\t')
+            {
+                count++;
+            }
+        }
+        //check to see if the file ends in a space
+        //(don't count that as a new word)
+        if(entry[entry.Length - 1] == ' ')
+        {
+            count--;
+        }
+        //check to see if someone put a space before a symbol
+        if(entry[entry.Length - 1] == '!' || entry[entry.Length - 1] == '?' || entry[entry.Length - 1] == '.')
+        {
+            if(entry[entry.Length - 2] == ' ')
+            {
+                count--;
+            }
+        }
+        return count;
+    }
+    public int char_count()
+    {
+         int count = 1;
+        for(int i = 0; i < entry.Length; i++)
+        {
+            count++;
+        }
+        if(entry[entry.Length - 1] == ' ')
+        {
+            count--;
+        }
+        return count;   
     }
 }
