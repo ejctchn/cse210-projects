f5d8e814a5d664f5b9741447a8778fd7466225ef
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-05-25 16:14:09 -0600

    scripture program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Scriptures scripture = new Scriptures();
+        
         Console.WriteLine("Hello Develop03 World!");
     }
 }
---
+++ prove/Develop03/Scriptures.cs
@@ -0,0 +1,14 @@
+public class Scriptures
+{
+    public string [] scripts = 
+    {
+        // 1 Nephi 3:7
+        "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, " +
+        "for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that " +
+        "they may accomplish the thing which he commandeth them.",
+        // Jacob 2:18-19
+        "But before ye seek for riches, seek ye for the kingdom of God.",
+        "And after ye have obtained a hope in Christ ye shall obtain riches, if ye seek them; and ye will seek them for the intent " +
+        "to do good—to clothe the naked, and to feed the hungry, and to liberate the captive, and administer relief to the sick and the afflicted."
+    };
+}
